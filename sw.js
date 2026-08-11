3360ccd57ef2a3f44eda9adcd8b8b5a16193718e
alanclebersantana <alanclebersantana@gmail.com>
2026-08-11 16:02:30 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker — Territórios */
-const CACHE = 'territorios-v11';
+const CACHE = 'territorios-v12';
 const SHELL = ['./index.html','./manifest.json','./mapas/geral.png',
   './icon-96.png','./icon-192.png','./icon-384.png','./icon-512.png',
   './icon-192-maskable.png','./icon-512-maskable.png','./icon-32.png'];
